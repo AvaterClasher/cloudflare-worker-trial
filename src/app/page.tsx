a899d00f5788386f5bbec8bc29c4eaeb7d9ece9c
AvaterClasher <116944847+AvaterClasher@users.noreply.github.com>
2024-10-01 18:44:05 +0530

    added api call

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
-import Image from "next/image";
+"use client"
+
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [name, setName] = useState(""); 
+
+  useEffect(() => {
+    const fetchName = async () => {
+      const response = await fetch("/api/hello");
+      const text = await response.text(); 
+      setName(text); 
+    };
+
+    fetchName();
+  }, []);
+
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-screen py-2">
         <h1 className="text-3xl font-bold">
-          Hi from<span className="text-red-300 pl-2">Soumyadip Moni 🔥</span>
+          Hi from<span className="text-red-300 pl-2">{name} 🔥</span>
         </h1>
       </div>
     </>
